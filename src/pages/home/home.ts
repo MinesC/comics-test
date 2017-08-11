bdb4578bd8e5864857d17b231cdc540a73d2f696
María Inés Carvajal <mines.carvajalc@gmail.com>
2017-08-11 12:30:21 -0400

    Se agrego función para el infiniteScrolling Se modifico home.html para agregarle componente

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,25 +10,46 @@ import { DetailPage } from '../detail/detail';
   templateUrl: 'home.html'
 })
 export class HomePage {
+
 	comics: any;
+	publicKey = '96558ca279bf14f5aead546ced0cdd2b';
+	baseUrl = 'https://gateway.marvel.com/v1/public/comics?';
+	ts = new Date().getTime();
+	limit = 20;
+	offset = 0;
+	md = Md5.hashStr(this.ts+'acabe8a3a268841c5fb1499bfbcd43829ee8b1e3'+this.publicKey);
+  encodedPath = this.baseUrl +'&apikey=' + this.publicKey + '&hash='+ this.md+ '&ts='+this.ts + '&limit='+ this.limit;
 
 
   constructor(public navCtrl: NavController, public http: Http) {
+  	this.comicList();
+  }
 
-				let publicKey = '96558ca279bf14f5aead546ced0cdd2b';
-				let baseUrl = 'https://gateway.marvel.com/v1/public/comics?';
-				let ts = new Date().getTime();
-				let hash = Md5.hashStr(ts+'acabe8a3a268841c5fb1499bfbcd43829ee8b1e3'+publicKey);
-        let encodedPath = baseUrl +'&apikey=' + publicKey + '&hash='+ hash+ '&ts='+ts;
 
-        this.http.get(encodedPath)
+  comicList(){
+  
+  	        this.http.get(this.encodedPath)
             .map(res => res.json()).subscribe(data => {
                 this.comics = data.data.results;
-                console.log(this.comics);
             },
             err => {
                 console.log('error in MCU');
             });
+ 
+  }
+
+  comicLoadMore(infiniteScroll){
+ 						this.offset = this.offset + this.limit;
+ 						let offsetPath = this.encodedPath +'&offset=' +this.offset
+  	  	    this.http.get(offsetPath)
+            .map(res => res.json()).subscribe(data => {
+                this.comics = this.comics.concat(data.data.results);
+                infiniteScroll.complete();
+            },
+            err => {
+                console.log('error in MCU');
+            });
+
   }
 
   comicSelected(comic){
